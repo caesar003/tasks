4d963f1f96af546ce6c85fd1fd50b81875235942
caesar003 <caesarmuksid@gmail.com>
2022-09-13 19:28:17 +0700

    delete disabled for non ownere

--- app/routes/projects/$projectId.tsx
+++ app/routes/projects/$projectId.tsx
@@ -3,13 +3,21 @@ import {db} from '~/utils/db.server';
 
 import {redirect} from '@remix-run/node';
 import {useLoaderData} from '@remix-run/react';
+import {getUser} from '~/utils/session.server';
 
 export const loader = async ({params, request}) => {
     const project = await db.project.findUnique({
-        where: {id: params.projectId},
+        where: {
+            id: params.projectId,
+        },
+        include: {
+            user: true,
+        },
     });
+    const loggedInUser = await getUser(request);
 
-    const data = {params, project};
+    // console.log(loggedInUser);
+    const data = {params, project, user: loggedInUser};
     if (!project) return redirect('/notfound');
     return data;
 };
@@ -17,23 +25,27 @@ export const loader = async ({params, request}) => {
 export const action = async ({request}) => {
     const form = await request.formData();
     const id = form.get('id');
-    await db.project.delete({
-        where: {id: id},
-    });
-    // console.log(id);
+    const userId = form.get('userId');
+    const user = await getUser(request);
+
+    if (user.id === userId)
+        await db.project.delete({
+            where: {id: id},
+        });
     return redirect('/projects');
 };
 
 export default function Project() {
     const [isModalOpen, openModal] = useState(false);
-    const {params, project} = useLoaderData();
+    const {params, project, user} = useLoaderData();
     const {
         id: d,
         name: na,
-        createdBy: by,
+        userId: by,
         description: ds,
         createdAt: at,
         updatedAt: up,
+        user: U,
     } = project;
     return (
         <>
@@ -41,7 +53,7 @@ export default function Project() {
             <p>{ds}</p>
             <p>
                 <em>
-                    by {by}, {at}
+                    by {U.username}, {at}
                 </em>
             </p>
             <h2 className='text-2xl'>Tasks</h2>
@@ -52,7 +64,14 @@ export default function Project() {
                     <del>task three</del>
                 </li>
             </ol>
-            <button onClick={() => openModal(true)}>Delete</button>
+            {user.id === by && (
+                <button
+                    className='btn bg-peach '
+                    onClick={() => openModal(true)}
+                >
+                    Delete
+                </button>
+            )}
             {isModalOpen && (
                 <form method='post'>
                     <div className='modal-background'>
@@ -75,6 +94,7 @@ export default function Project() {
                             </div>
                             <div className='modal-footer'>
                                 <input type='hidden' name='id' value={d} />
+                                <input type='hidden' name='userId' value={by} />
                                 <button
                                     onClick={() => openModal(false)}
                                     className='btn btn-cancel'
@@ -82,6 +102,7 @@ export default function Project() {
                                 >
                                     Cancel
                                 </button>
+
                                 <button
                                     className='btn btn-delete'
                                     type='submit'
