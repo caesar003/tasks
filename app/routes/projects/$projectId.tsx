95a6380d44e7a04e1c352718ef851b16c598392c
caesar003 <caesarmuksid@gmail.com>
2022-09-14 23:05:35 +0700

    updating status

--- app/routes/projects/$projectId.tsx
+++ app/routes/projects/$projectId.tsx
@@ -105,6 +105,16 @@ export const action = async ({request}) => {
             return redirect(`/projects/${projectId}`);
         }
 
+        case 'updateTaskStatus': {
+            const id = form.get('taskId');
+            const status = form.get('taskStatus');
+            await db.task.update({
+                data: {status},
+                where: {id},
+            });
+            return redirect(`/projects/${projectId}`);
+        }
+
         default: {
             return {};
         }
@@ -141,6 +151,19 @@ export default function Project() {
         return new Date(date)
             .toJSON()
             .slice(0, new Date(date).toJSON().indexOf('.'));
+    };
+
+    const statusToUpdate = (currentStatus) => {
+        let res = '';
+        switch (currentStatus) {
+            case 'Not Started':
+                res = 'In Progress';
+                break;
+            case 'In Progress':
+                res = 'Done';
+                break;
+        }
+        return res;
     };
 
     return (
@@ -268,25 +291,117 @@ export default function Project() {
                                     <div>Status: {task.status}</div>
                                 </div>
                                 <div>
-                                    <button
-                                        title='Start'
-                                        className='block bg-limeGreen hover:bg-green-600 text-black hover:text-gray-400 m-1 p-0.5 rounded border border-slate-700'
-                                    >
-                                        <svg
-                                            xmlns='http://www.w3.org/2000/svg'
-                                            fill='none'
-                                            viewBox='0 0 24 24'
-                                            strokeWidth={1.5}
-                                            stroke='currentColor'
-                                            className='w-6 h-6'
-                                        >
-                                            <path
-                                                strokeLinecap='round'
-                                                strokeLinejoin='round'
-                                                d='M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z'
-                                            />
-                                        </svg>
-                                    </button>
+                                    <div>
+                                        {task.status === 'Done' ? (
+                                            <span className='block bg-blue-200 hover:bg-green-600 text-black hover:text-gray-400 m-1 p-0.5 rounded border border-slate-700'>
+                                                <svg
+                                                    xmlns='http://www.w3.org/2000/svg'
+                                                    fill='none'
+                                                    viewBox='0 0 24 24'
+                                                    strokeWidth={1.5}
+                                                    stroke='currentColor'
+                                                    className='w-6 h-6'
+                                                >
+                                                    <path
+                                                        strokeLinecap='round'
+                                                        strokeLinejoin='round'
+                                                        d='M4.5 12.75l6 6 9-13.5'
+                                                    />
+                                                </svg>
+                                            </span>
+                                        ) : task.status === 'In Progress' ? (
+                                            <form method='post'>
+                                                <input
+                                                    type='hidden'
+                                                    name='requestType'
+                                                    value='updateTaskStatus'
+                                                />
+                                                <input
+                                                    type='hidden'
+                                                    name='projectId'
+                                                    value={projectId}
+                                                />
+                                                <input
+                                                    type='hidden'
+                                                    name='taskId'
+                                                    value={task.id}
+                                                />
+                                                <input
+                                                    type='hidden'
+                                                    name='taskStatus'
+                                                    value={statusToUpdate(
+                                                        task.status,
+                                                    )}
+                                                />
+                                                <button
+                                                    type='submit'
+                                                    title='Start'
+                                                    className='block bg-blue-400 hover:bg-green-600 text-black hover:text-gray-400 m-1 p-0.5 rounded border border-slate-700'
+                                                >
+                                                    <svg
+                                                        xmlns='http://www.w3.org/2000/svg'
+                                                        fill='none'
+                                                        viewBox='0 0 24 24'
+                                                        strokeWidth={1.5}
+                                                        stroke='currentColor'
+                                                        className='w-6 h-6'
+                                                    >
+                                                        <path
+                                                            strokeLinecap='round'
+                                                            strokeLinejoin='round'
+                                                            d='M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z'
+                                                        />
+                                                    </svg>
+                                                </button>
+                                            </form>
+                                        ) : (
+                                            <form method='post'>
+                                                <input
+                                                    type='hidden'
+                                                    name='requestType'
+                                                    value='updateTaskStatus'
+                                                />
+                                                <input
+                                                    type='hidden'
+                                                    name='taskId'
+                                                    value={task.id}
+                                                />
+                                                <input
+                                                    type='hidden'
+                                                    name='projectId'
+                                                    value={projectId}
+                                                />
+                                                <input
+                                                    type='hidden'
+                                                    name='taskStatus'
+                                                    value={statusToUpdate(
+                                                        task.status,
+                                                    )}
+                                                />
+                                                <button
+                                                    type='submit'
+                                                    title='Start'
+                                                    className='block bg-limeGreen hover:bg-green-600 text-black hover:text-gray-400 m-1 p-0.5 rounded border border-slate-700'
+                                                >
+                                                    <svg
+                                                        xmlns='http://www.w3.org/2000/svg'
+                                                        fill='none'
+                                                        viewBox='0 0 24 24'
+                                                        strokeWidth={1.5}
+                                                        stroke='currentColor'
+                                                        className='w-6 h-6'
+                                                    >
+                                                        <path
+                                                            strokeLinecap='round'
+                                                            strokeLinejoin='round'
+                                                            d='M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z'
+                                                        />
+                                                    </svg>
+                                                </button>
+                                            </form>
+                                        )}
+                                    </div>
+
                                     <button
                                         title='Edit'
                                         className='block bg-aquaMarine border border-slate-700 m-1 p-0.5 rounded'
